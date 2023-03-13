939eeab54a53298999e0e03d7c788bac7ac8d3cc
Mario Pescarmona <mpescarmona@gmail.com>
2023-03-13 16:32:30 -0300

    Add utility methods to Money valueObject into common-domain module

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -1,6 +1,7 @@
 package com.food.ordering.system.domain.valueobject;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Objects;
 
 /**
@@ -11,6 +12,26 @@ public class Money {
 
     public Money(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public boolean isGreaterThanZero() {
+        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreaterThan(Money money) {
+        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+    public Money add(Money money) {
+        return new Money(setScale(this.amount.add(money.getAmount())));
+    }
+
+    public Money substract(Money money) {
+        return new Money(setScale(this.amount.subtract(money.getAmount())));
+    }
+
+    public Money multiply(int multiplier) {
+        return new Money(setScale(this.amount.multiply(new BigDecimal(multiplier))));
     }
 
     public BigDecimal getAmount() {
@@ -28,5 +49,9 @@ public class Money {
     @Override
     public int hashCode() {
         return Objects.hash(amount);
+    }
+
+    private BigDecimal setScale(BigDecimal input) {
+        return input.setScale(2, RoundingMode.HALF_EVEN);
     }
 }
