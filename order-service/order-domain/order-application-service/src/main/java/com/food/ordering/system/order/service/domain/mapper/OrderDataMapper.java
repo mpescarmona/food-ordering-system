afe3d302402961f7a531b699031f62d31553ed7b
Mario Pescarmona <mpescarmona@gmail.com>
2023-03-14 14:25:47 -0300

    Adding Mapper class in Order Application Service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
@@ -0,0 +1,10 @@
+package com.food.ordering.system.order.service.domain.mapper;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: Mario Oscar Pescarmona Tricoli
+ */
+@Component
+public class OrderDataMapper {
+}
