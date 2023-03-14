8d6f959d5492c52358fd1a7899aa443e3bb7b62a
Mario Pescarmona <mpescarmona@gmail.com>
2023-03-14 14:17:16 -0300

    Implement Domain Events in Order Service domain layer

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderEvent.java
@@ -0,0 +1,27 @@
+package com.food.ordering.system.order.service.domain.event;
+
+import com.food.ordering.system.domain.event.DomainEvent;
+import com.food.ordering.system.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author: Mario Oscar Pescarmona Tricoli
+ */
+public abstract class OrderEvent implements DomainEvent<Order> {
+    private final Order order;
+    private final ZonedDateTime createdAt;
+
+    public OrderEvent(Order order, ZonedDateTime createdAt) {
+        this.order = order;
+        this.createdAt = createdAt;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
