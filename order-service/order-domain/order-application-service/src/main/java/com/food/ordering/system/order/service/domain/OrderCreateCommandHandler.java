5e90d9945768cf5fe160c035290932f3219e11ca
Mario Pescarmona <mpescarmona@gmail.com>
2023-04-18 19:33:29 -0400

    Adding support for set message in OrderDataMapper class

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -31,6 +31,6 @@ public class OrderCreateCommandHandler {
         log.info("Order created with id: {}", orderCreatedEvent.getOrder().getId().getValue());
         orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
 
-        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder());
+        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder(), "Order created Successfully");
     }
 }
