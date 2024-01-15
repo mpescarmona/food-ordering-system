be08083cb8ed24c118b700cdecccbedc530d1aab
Mario Pescarmona <mpescarmona@gmail.com>
2024-01-15 19:40:30 -0300

    Fixing module dependency hierarchy and ErrorDTO dependency imports

--- order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/exception/handler/OrderGlobalExceptionHandler.java
+++ order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/exception/handler/OrderGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.food.ordering.system.order.service.application.exception.handler;
 
+import com.food.ordering.system.application.handler.ErrorDTO;
 import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
 import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
 import lombok.extern.slf4j.Slf4j;
