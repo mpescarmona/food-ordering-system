a0c70ef65f82d99f11696b91c5201209233bdacd
Mario Pescarmona <mpescarmona@gmail.com>
2024-01-22 16:47:21 -0300

    Add data access mapper for customer in order-dataaccess module

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
@@ -7,4 +7,10 @@ import com.food.ordering.system.domain.valueobject.CustomerId;
  * @author: Mario Oscar Pescarmona Tricoli
  */
 public class Customer extends AggregateRoot<CustomerId> {
+    public Customer() {
+    }
+
+    public Customer(CustomerId customerId) {
+        super.setId(customerId);
+    }
 }
