3462c96a0c1723db5645263d36ccb7867a3662d7
Mario Pescarmona <mpescarmona@gmail.com>
2024-01-22 18:01:03 -0300

    Add data access mapper for restaurant in order-dataaccess module

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/exception/RestaurantDataAccessException.java
@@ -0,0 +1,11 @@
+package com.food.ordering.system.order.service.dataaccess.restaurant.exception;
+
+/**
+ * @author: Mario Oscar Pescarmona Tricoli
+ */
+public class RestaurantDataAccessException extends RuntimeException {
+
+    public RestaurantDataAccessException(String message) {
+        super(message);
+    }
+}
